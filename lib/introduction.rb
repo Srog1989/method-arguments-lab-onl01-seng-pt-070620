42288cd823cad28a48274a306c80812f49d7f963
Sarah Rogers <s.rogers0715@yahoo.com>
2020-07-08 02:41:49 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,7 +6,7 @@ end
 introduction ("Sarah")
 
 def introduction_with_language (name, language)
-  puts "Hi, my name is #{name}" and I am learning to program in #{language}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
-introduction_with_language (sarah, ruby)
+introduction_with_language (sarah,ruby)
   
